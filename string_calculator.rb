75baa35bd8bebd2e152f71bd4ce723105e08fb55
Dani Latorre <dani@danilat.com>
2020-02-23 13:19:50 +0100

    Refactoring the current implementation to segregated classes

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,27 +1,61 @@
 class StringCalculator
-  DEFAULT_SEPARATOR = /[\n,]/
-  CUSTOM_SEPARATOR_INIT = "//"
-  CUSTOM_SEPARATOR_END = "\n"
   def add(input)
     result = 0
-    input_parser(input).split(@separator).each do |value|
-      result += value.to_i
+    numbers_parser = NumbersParser.new(input)
+    numbers_parser.number_values.each do |value|
+      result += value
     end
-    return result
+    result
+  end
+end
+
+class NumbersParser
+  def initialize(input)
+    if CustomValuesSeparator.custom?(input)
+      @values_separator = CustomValuesSeparator.new(input)
+    else
+      @values_separator = DefaultValuesSeparator.new(input)
+    end
   end
 
-  def input_parser(input)
-    @separator = DEFAULT_SEPARATOR
-    if custom_separator?(input)
-      input_parts = input.split(CUSTOM_SEPARATOR_INIT)
-      @separator = input_parts[1].split(CUSTOM_SEPARATOR_END)[0]
-      input = input_parts[1].split(CUSTOM_SEPARATOR_END)[1]
-    end
-    return input
+  def number_values
+    @values_separator.values.collect(&:to_i)
+  end  
+end
+
+class CustomValuesSeparator
+  CUSTOM_SEPARATOR_INIT = "//"
+  CUSTOM_SEPARATOR_END = "\n"
+
+  def self.custom?(input)
+    input.start_with?(CUSTOM_SEPARATOR_INIT) && input.include?(CUSTOM_SEPARATOR_END)
   end
 
-  def custom_separator?(input)
-    input_parts = input.split(CUSTOM_SEPARATOR_INIT)
-    return input_parts.size > 1
+  def initialize(input)
+    @separator_and_numbers = input.slice(CUSTOM_SEPARATOR_INIT.size..-1)
+  end
+
+  def values
+    values_joined.split(separator)
+  end
+
+  private def separator
+    @separator_and_numbers.split(CUSTOM_SEPARATOR_END)[0]
+  end
+
+  private def values_joined
+    @separator_and_numbers.split(CUSTOM_SEPARATOR_END)[1]
+  end
+end
+
+class DefaultValuesSeparator
+  DEFAULT_SEPARATION = /[\n,]/
+
+  def initialize(input)
+    @input = input
+  end
+
+  def values
+    @input.split(DEFAULT_SEPARATION)
   end
 end
